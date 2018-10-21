4609292738c3f13c2afc4d4b4b01803b5de5dc35
Vikkikikie <Ciel30rus@mail.ru>
2018-10-21 18:22:28 +0300

    task-01 is completed

---
+++ src/ex6_js-array-methods/task-01.js
@@ -0,0 +1,30 @@
+'use strict'
+function slice (array, begin, end) {
+    var arrayf = [];
+    var beginf = begin;
+    var endf = end;
+    if (arguments.length === 3){
+        if (beginf>=0 && endf>=0){
+            for (beginf;beginf<endf;beginf++){
+                arrayf.push(array[beginf]);
+            };
+        } else {
+            for (beginf;array.length-beginf>array.length-endf;beginf--){
+                arrayf.push(array[beginf]);
+            };
+        };
+    } else if (arguments.length === 2){
+        if (beginf>=0 && endf>=0){
+            for (beginf;beginf<array.length;beginf++){
+                arrayf.push(array[beginf]);
+            };
+        } else {
+            for (beginf;array.length-beginf>=0;beginf--){
+                arrayf.push(array[beginf]);
+            };
+        };
+    } else {
+        arrayf = array;
+    };
+};
+module.exports = slice;
