bf2cf913ba72bb54c50558b381ff658e32a55d67
Vikkikikie <Ciel30rus@mail.ru>
2018-10-27 17:12:12 +0300

    Home_work_is_completed

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -9,17 +9,17 @@ function slice (array, begin, end) {
                 arrayf.push(array[beginf]);
             };
         } else {
-            for (beginf;array.length-beginf>array.length-endf;beginf--){
+            for (beginf;array.length+beginf>array.length+endf;beginf--){
                 arrayf.push(array[beginf]);
             };
         };
     } else if (arguments.length === 2){
-        if (beginf>=0 && endf>=0){
+        if (beginf>=0){
             for (beginf;beginf<array.length;beginf++){
                 arrayf.push(array[beginf]);
             };
         } else {
-            for (beginf;array.length-beginf>=0;beginf--){
+            for (beginf;array.length+beginf>=0;beginf--){
                 arrayf.push(array[beginf]);
             };
         };
